21c3bffdb9f4eefa2e166418f256787ec08e5f5c
emper911 <emper96@gmail.com>
2017-01-05 21:48:32 -0500

    Add files via upload

---
+++ autoGarden.py
@@ -0,0 +1,89 @@
+import time
+import RPi.GPIO as GPIO
+from gpiozero import LED, Button
+
+#GPIO pins
+motor = LED(27)
+lights = LED(17)
+ONOFF_Button = Button(26)
+motorTime_Button = Button(23)
+lightTime_Button = Button(24)
+start = False #when button is pressed to begin
+#light times
+hrs15 = 54000.00
+hrs12  = 43200.00
+hrs13 = 46800.00
+hrs24 = 86400.00
+
+#light time default is 13 hrs
+hrs = 46800.00
+interval = 3600.00 #incriments of 1 hr
+
+
+#motor times default is 1 min on and 4 minutes off
+tMotorOn = 60.00
+tMotorOff = 240.00
+t30sec = 30.00 #increments of 30 seconds
+
+motorTime = 0
+currentTime = 0
+LEDTime = 0
+
+def LEDTimer():
+    global LEDTime
+    if (time.time() - LEDTime) < hrs:
+        lights.on()
+    elif (time.time() - LEDTime) < hrs24:
+        lights.off()
+    else:
+        LEDTime = time.time()
+
+def motorTimer():
+    global motorTime
+    global LEDTime
+    if (time.time() - motorTime) < tMotorOn:
+        motor.on()
+    elif (time.time() - motorTime) < tMotorOff:
+        motor.off()
+    else:
+        printer(LEDTime)
+        motorTime = time.time()
+
+def printer(Time):
+    print("{0:06.2f} LED time\n".format(time.time() - Time))
+
+def blueToothHandler():
+    global tMotorOn  #motor time On
+    global tMotorOff #motor time Off
+    global hrs       #time light is on
+    global start     #boolean that is ON/OFF switch
+
+    #must send information - TX: humidity, temperature, tMotorOn, tMotorOff, hrs
+    
+    #must receive information - RX: tMotorOn, tMotorOff, hrs, start(make a kill switch on the bluetooth device)
+    
+
+while True:
+    motor.off()
+    lights.off()
+    #when program is turned on waiting for button to be pressed to turn on garden
+    if ONOFF_Button.is_pressed:
+        time.sleep(1)
+        print("Garden is On")
+        start = True
+        currentTime = time.time()
+        LEDTime = time.time()
+        motorTime = time.time()
+    #when button is pressed program starts
+    while start:
+        #if button is pressed again program terminates into user determined loop
+
+        LEDTimer()
+        motorTimer()
+
+        if ONOFF_Button.is_pressed:
+            time.sleep(1)
+            start = False
+            print("Garden is Off")
+
+
